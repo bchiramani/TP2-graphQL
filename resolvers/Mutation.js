b2f425206918c790fdc02a55fd131aefd832c27b
bchiramani <amanibchir00@gmail.com>
2022-04-27 16:08:54 +0100

    mise a jour

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,18 +1,41 @@
 export const Mutation = {
-    addTodo: (parent, { addTodoInput }, { db }, info) => {
-        const newTodo = { id: String, ...addTodoInput }
-        db.todo.push(newTodo);
-        //pubsub.publish('newTodo', { newTodo })
-        return newTodo
+    addTodo: (parent, { addTodoInput }, { db, pubsub }, infos) => {
+        if (!existInArray(db.users, "id", addTodoInput.user)) {
+            throw new Error("not such a user . Please verify")
+        } else {
+            const newTodo = { id: db.todos.length ? db.todos[db.todo.length - 1].id + 1 : 1, ...addTodoInput }
+            db.todos.push(newTodo);
+            pubsub.publish('todo', { todo: { newTodo, mutation: "ADD" } })
+            return newTodo
+        }
+
     },
-    editTodo: (parent, { editTodoInput }, { db }, info) => {
-        const newTodo = { id: String, ...editTodoInput }
-        db.todo.patch(newTodo);
-        //pubsub.publish('newTodo', { newTodo })
-        return newTodo
+    editTodo: (parent, { id, editTodoInput }, { db, pubsub }, info) => {
+        if (editTodoInput.user && !existInArray(db.users, "id", editTodoInput.user)) {
+            throw new Error("not such a user . Please verify");
+        } else {
+            //Il faut que l'id du todo existe
+            const todo = db.todos.find((todoItem) => todoItem.id === id);
+            if (!todo) {
+                throw new Error("todo n existe pas");
+            } else {
+                for (let key in editTodoInput) {
+                    todo[key] = editTodoInput[key];
+                }
+                pubsub.publish("todo", { todo: { todo, mutation: "UPDATE" } });
+                return todo;
+            }
+        }
     },
     deleteTodo: (parent, { id }, { db }, info) => {
-        db.todo.delete(id);
+        const myTodo = db.todos.findIndex((todo) => todo.id === id);
+        if (myTodo === -1) {
+            throw new Error("Todo innexistant");
+        } else {
+            const [todo] = db.todos.splice(myTodo, 1);
+            pubsub.publish("todo", { todo: { todo, mutation: "DELETE" } });
+            return todo;
+        }
+    },
 
-    }
 }
